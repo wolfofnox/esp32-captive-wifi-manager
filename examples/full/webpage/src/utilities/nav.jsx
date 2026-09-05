99e4563ef168d41802fee1125a63456552c80e73
wolfofnox <137057355+wolfofnox@users.noreply.github.com>
2026-09-05 13:51:25 +0200

    full examplle fixed preact restart not triggering POST

--- examples/full/webpage/src/utilities/nav.jsx
+++ examples/full/webpage/src/utilities/nav.jsx
@@ -7,7 +7,15 @@ export default function Nav() {
         <a href="/control">HTML Post control</a>
         <a href="/web-socket">Web Socket</a>
         <a href="/captive">Captive Portal</a> {/* Captive Portal is accessible even when the device is not connected to a Wi-Fi network */}
-        <a href="#" onclick="fetch('/restart', {method: 'POST'}).then(response => console.log(response)).then(() => window.location.reload());">Restart</a> {/* Restart the device */}
+        <a href="#"
+          onClick={(event) => {
+            event.preventDefault();
+
+            fetch("/restart", { method: "POST" })
+              .then((response) => console.log(response))
+              .then(() => window.location.reload());
+          }}
+        >Restart Device</a> {/* Restart the device */}
         {/* Add more links as needed */}
       </nav>
     )
